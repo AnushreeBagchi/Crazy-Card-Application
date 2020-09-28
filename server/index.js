631b168480881b98e0eadc500e52c0d7976ca5da
Anushree Bagchi <anushreebagchi4791@gmail.com>
2020-09-28 22:46:30 +0530

    bug fixed

--- server/index.js
+++ server/index.js
@@ -3,10 +3,11 @@ var express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
 var app = express();
+var cors = require('cors')
+
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-
 
 router.get('/cards', (req, res)=>{
     const body = req.body;
